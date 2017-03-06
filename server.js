61304b3d343efb94f510467b6b175ec64f8b912e
Justin Qassis <qassisju@msu.edu>
2017-03-05 20:24:40 -0500

    updated random jokes page

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 
 var http = require('http')
 
-var randomJokeChooser = function () {
+var server = http.createServer(function (request, response) {
   var jokeOne = 'Knock, knock! Who’s there? Opportunity! That is impossible. Opportunity doesn’t come knocking twice!'
   var jokeTwo = 'Knock knock. Whos there? Beats. Beats who? Beats me.'
   var jokeThree = 'Knock knock. Who’s there?The door.'
   var randNum = Math.random()
-  if (randNum > 0.0 && randNum < 0.4) {
-    console.log(jokeOne)
-  }
-  if (randNum > 0.4 && randNum < 0.7) {
-    console.log(jokeTwo)
-  }
-  if (randNum > 0.7 && randNum < 1) {
-    console.log(jokeThree)
-  }
-}
-
-var server = http.createServer(function (request, response) {
   if (request.url === '/') {
     response.end('Welcome to my server')
   } else if (request.url === '/random-joke') {
-    randomJokeChooser()
+    if (randNum > 0.0 && randNum < 0.4) {
+      response.end(jokeOne)
+    }
+    if (randNum > 0.4 && randNum < 0.7) {
+      response.end(jokeTwo)
+    }
+    if (randNum > 0.7 && randNum < 1) {
+      response.end(jokeThree)
+    }
   } else if (request.url === '/cuteness') {
     response.end(
       '<h1>Cute animal</h1>' +
